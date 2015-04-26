4e5f43497a3842a3c44eb05a490b5c4cda2e0937
Conor Flannigan <cflanni1@jhu.edu>
2015-04-26 13:20:52 -0700

    Refactor file structure for increased modularity

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
-// Our "geo-proximity" library runs on a redis cache
-var redis = require('redis');
-var client = redis.createClient();
-
-// We'll communicate with our client in real-time using socket.io
-// wrapped around an express server.
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-// Initialize the proximity module with our redis client
-var proximity = require('geo-proximity').initialize(client);
+// Grab our http wrapper and express server
+var http = require('./lib/server').http;
+var app = require('./lib/server').app;
 
 // Handler includes logic for user/cache lifecycle events
 var handler = require('./lib/handler');
 
+// Set up routing for web socket events
+require('./lib/router');
+
 // Set up a basic webpage to serve for testing purposes
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
-});
-
-// Listen for user connections via sockets
-io.on('connection', function(socket) {
-  console.log('A user connected');
-
-  // Handle radial query registration
-  socket.on('add_query', handler.addQuery);
-
-  // Handle radial query removal
-  socket.on('remove_query', handler.removeQuery);
-
-  // Handle key registration and updating
-  socket.on('set_key', handler.setKey);
-
-  // Handle key removal
-  socket.on('remove_key', handler.removeKey);
-
-  // Handle client disconnect
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
-  });
 });
 
 // Start up the server
