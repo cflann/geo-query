f845af46adb76e21dd3f70fb4d8ddfb474f1b6e7
Conor Flannigan <cflanni1@jhu.edu>
2015-04-26 11:50:26 -0700

    (feat) Set up skeleton for geoquery event API

---
+++ index.js
@@ -0,0 +1,52 @@
+// Our "geo-proximity" library runs on a redis cache
+var redis = require('redis');
+var client = redis.createClient();
+
+// We'll communicate with our client in real-time using socket.io
+// wrapped around an express server.
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// Initialize the proximity module with our redis client
+var proximity = require('geo-proximity').initialize(client);
+
+// Handler includes logic for user/cache lifecycle events
+var handler = require('lib/handler');
+
+// Set up a basic webpage to serve for testing purposes
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Listen for user connections via sockets
+io.on('connection', function(socket) {
+  console.log('A user connected');
+
+  // Handle radial query registration
+  socket.on('add_query', function(id, coords, radius) {
+  });
+
+  // Handle radial query removal
+  socket.on('remove_query', function(id) {
+  });
+
+  // Handle key registration
+  socket.on('add_key', function(id, coords) {
+  });
+
+  // Handle key removal
+  socket.on('remove_key', function(id) {
+  });
+
+  // Handle client disconnect
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+});
+
+// Start up the server
+http.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
+
